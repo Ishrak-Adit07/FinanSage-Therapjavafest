c325081756fb4c4ce5fa8ef580e136d864171f80
Ishrak Adit <ishrak.adit07@gmail.com>
2024-07-11 12:36:19 +0600

    Client wallet creation function file revise commit

---
+++ client/src/sections/wallets/wallets/WalletCreate.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { motion } from "framer-motion";
+import Subscribe from '../../../components/buttons/Subscribe';
+
+const gridSquareVariants = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1 }
+  };
+
+const WalletCreate = () => {
+
+  const handleCreateWalletButton = () =>{
+    console.log("Go to create wallet page");
+  }
+
+  return (
+      <div className='layoutSection text-slate-200 border-b border-neutral-900 pb-4'>
+      <motion.div 
+        variants={gridSquareVariants}
+        className='flex flex-wrap items-center justify-center'
+      >
+        <motion.div 
+          className='w-full py-4 lg:p-10'
+          initial={{ opacity: 0, y: -10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, ease: "easeOut", delay: 0.4 }}
+        >
+          <div className='flex justify-center items-center w-full'>
+            <Subscribe text={"Create new wallet"} onClickAction={handleCreateWalletButton}/>
+          </div>
+        </motion.div >
+      </motion.div>
+      </div>
+  );
+}
+
+export default WalletCreate;
