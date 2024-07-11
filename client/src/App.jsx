e16cec93d37e72d7ee228e2f01826b9a60cdfa82
Ishrak Adit <ishrak.adit07@gmail.com>
2024-07-11 12:50:43 +0600

    Client intermediate App.jsx file revise commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
+import AuthRoutes from './routesProtection/AuthRoutes';
+import GuestRoutes from './routesProtection/GuestRoutes';
 
+import Layout from './pages/Layout';
+import Login from './pages/users/Login';
+import Register from './pages/users/Register';
+import Home from './pages/Home';
+import Dashboard from './pages/users/Dashboard';
+import Wallets from './pages/Financials/wallets/Wallets';
+import Wallet from './pages/Financials/wallets/Wallet';
+import UserTransactions from './pages/Financials/transaction/UserTransactions';
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <BrowserRouter>
 
-export default App
+      <Routes>
+
+        <Route path="/" element={<Layout />} >
+
+          <Route index element={<Home />} />
+
+          <Route element={<AuthRoutes />}>
+    
+            <Route path="user/dashboard" element={<Dashboard />} />
+            <Route path="user/wallets" element={<Wallets />} />
+            <Route path="/walletDetails/:title" element={<Wallet />} />
+            <Route path="user/transactions" element={<UserTransactions />} />
+
+          </Route>
+
+          <Route element={<GuestRoutes />}>
+
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+
+          </Route>
+
+
+        </Route>
+
+      </Routes>
+
+    </BrowserRouter>
+  )
+};
+
+export default App;
