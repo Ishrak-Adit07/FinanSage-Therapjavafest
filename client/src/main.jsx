535e8945c7612954f55d4b24ab3c14df1e02b3fc
Ishrak Adit <ishrak.adit07@gmail.com>
2024-07-11 12:51:32 +0600

    Client intermediate main.jsx revise file commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './assets/app.css'
+
+import UserProvider from './contexts/UserContext.jsx'
+import BudgetProvider from './contexts/BudgetContext.jsx'
+import WalletProvider from './contexts/WalletContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    
+      <BudgetProvider>
+
+        <WalletProvider>
+
+          <UserProvider>
+
+            <App />
+
+          </UserProvider>
+
+        </WalletProvider>
+
+      </BudgetProvider>
+
+  </React.StrictMode>
 )
