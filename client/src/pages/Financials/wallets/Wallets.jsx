1b45c5637e3986e4fa6c8565d49d42fb963b6a6b
Ishrak Adit <ishrak.adit07@gmail.com>
2024-07-11 12:48:31 +0600

    Client user wallets page file revise commit

---
+++ client/src/pages/Financials/wallets/Wallets.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable no-unused-vars */
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import WalletGraph from '../../../sections/wallets/wallet/WalletGraph';
+import CashGraph from '../../../sections/finances/CashGraph';
+import WalletCreate from '../../../sections/wallets/wallets/WalletCreate';
+import WalletList from '../../../sections/wallets/wallets/WalletList';
+
+const Wallets = () => {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+
+    setTimeout(async()=>{
+
+      setLoading(false);
+    }, 1000);
+
+  }, []);
+
+  return (
+    <section className='card w-full'>
+
+        <div>
+
+          {loading && 
+            <i className="fa-solid fa-spinner animate-spin text-3xl text-center-block"></i>
+          }
+
+          {!loading &&
+            <div>
+              <CashGraph />
+              <CashGraph />
+              <WalletCreate />
+              <WalletList />
+            </div>
+          }
+        </div>
+
+    </section>
+  );
+}
+
+export default Wallets;
