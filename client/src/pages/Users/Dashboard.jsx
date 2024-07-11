aacbe560be12c72841e8139af677b0f0bab54096
Ishrak Adit <ishrak.adit07@gmail.com>
2024-07-11 14:56:42 +0600

    Client user dashboard file revise commit

---
+++ client/src/pages/Users/Dashboard.jsx
@@ -0,0 +1,56 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import React, { useContext, useEffect, useState } from 'react'
+import { UserContext } from '../../contexts/UserContext';
+import Success from '../../messages/Success';
+import Alert from '../../messages/Alert';
+
+const Dashboard = () => {
+
+  const {user, setUser} = useContext(UserContext)
+
+  const [loading, setLoading] = useState(true);
+
+  const [error, setError] = useState(null);
+  const [deleteSuccess, setDeleteSuccess] = useState(null);
+
+  useEffect(()=>{
+
+    setTimeout(async()=>{
+
+      setUser({
+        name: "Gale Hawthorne", 
+        email: "gale@dist12.com",
+      });
+
+      setLoading(false);
+    }, 1000);
+
+  }, []);
+
+  return (
+    <section className='card'>
+
+      <h1 className='title'>User Dashboard</h1>
+
+      <div>
+
+          {loading && 
+            <i className="fa-solid fa-spinner animate-spin text-3xl text-center-block"></i>
+          }
+
+          {!loading && 
+            <h1>This is user dashboard</h1>
+          }
+
+          {deleteSuccess && <Success msg={"Post was deleted"}/>}
+          {error && <Alert msg={error} />}
+      </div>
+
+
+
+    </section>
+  )
+}
+
+export default Dashboard;
