e261a0e6cbc142b06fb430968e9941698be55a99
Ishrak Adit <ishrak.adit07@gmail.com>
2024-07-11 12:18:07 +0600

    Client constant index file revise commit

---
+++ client/src/constants/index.js
@@ -0,0 +1,151 @@
+export const PROJECT_NAME = "FinanSage";
+
+export const PROJECT_NAME_CAP = "FINAN_SAGE";
+
+
+export const HERO_CONTENT = "This is FinanSage, your personal finance manager";
+
+export const USER_WALLETS = [
+    {
+      title: "Owltweet",
+      image: "Ot",
+      githubLink: "https://github.com/Ishrak-Adit07/Social-Media",
+      description:
+        "Web application for social media on harry potter theme with authentication.",
+      technologies: ["ReactJS", "MUI", "NodeJS", "ExpressJS", "MongoDB"],
+      balance: 275.75,
+    },
+    {
+      title: "HP Quiz-App",
+      image: "Qa",
+      githubLink: "https://github.com/Ishrak-Adit07/Quiz-app",
+      description:
+        "Web application for quiz on pre-stored questions.",
+      technologies: ["ReactJS", "NodeJS", "ExpressJS", "PostgreSQL"],
+      balance: 275.75,
+    },
+    {
+      title: "MERN Arch. Example Project",
+      image: "Mr",
+      githubLink: "https://github.com/Ishrak-Adit07/MERN-Todo",
+      description:
+        "A robust, fully functional MERN stack to-do list web app with user authentication and best coding practices.",
+      technologies: ["ReactJS", "tailwindcss", "NodeJS", "ExpressJS", "MongoDB"],
+      balance: 275.75,
+    },
+    {
+      title: "PERN Arch. Example Project",
+      image: "Pr",
+      githubLink: "https://github.com/Ishrak-Adit07/ToDoList",
+      description:
+        "A robust, fully functional PERN stack to-do list web app with user authentication and best coding practices.",
+      technologies: ["ReactJS", "tailwindcss", "NodeJS", "ExpressJS", "PostgreSQL"],
+      balance: 275.75,
+    },
+  ];
+
+  export const WALLET_INCOME_TYPES = [
+    {
+      name: "Salary",
+      icon: "", 
+    }, 
+    {
+      name: "Business",
+      icon: "", 
+    }, 
+    {
+      name: "Gifts",
+      icon: "", 
+    }, 
+    {
+      name: "Extra Income",
+      icon: "", 
+    }, 
+    {
+      name: "Loan",
+      icon: "", 
+    }, 
+    {
+      name: "Insurance",
+      icon: "", 
+    }, 
+    {
+      name: "Other",
+      icon: "", 
+    },
+  ];
+
+  export const WALLET_EXPENSE_TYPES = [
+    {
+      name: "Food & Drink",
+      icon: "", 
+    }, 
+    {
+      name: "Shopping",
+      icon: "", 
+    }, 
+    {
+      name: "Gifts",
+      icon: "", 
+    }, 
+    {
+      name: "Transport",
+      icon: "", 
+    }, 
+    {
+      name: "Home",
+      icon: "", 
+    }, 
+    {
+      name: "Bills & Fares",
+      icon: "", 
+    }, 
+    {
+      name: "Entertainment",
+      icon: "", 
+    },
+    {
+      name: "Car",
+      icon: "", 
+    }, 
+    {
+      name: "Travel",
+      icon: "", 
+    }, 
+    {
+      name: "Family",
+      icon: "", 
+    }, 
+    {
+      name: "Personal",
+      icon: "", 
+    }, 
+    {
+      name: "Healthcare",
+      icon: "", 
+    }, 
+    {
+      name: "Education",
+      icon: "", 
+    }, 
+    {
+      name: "Groceries",
+      icon: "", 
+    },
+    {
+      name: "Gifts",
+      icon: "", 
+    }, 
+    {
+      name: "Sports & Hobbies",
+      icon: "", 
+    }, 
+    {
+      name: "Work",
+      icon: "", 
+    }, 
+    {
+      name: "Others",
+      icon: "", 
+    },
+  ];
