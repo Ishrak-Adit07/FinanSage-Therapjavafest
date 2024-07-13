7dce6b7be3d427bac5f136997fce66807ef1e4c6
Ishrak Adit <ishrak.adit07@gmail.com>
2024-07-13 08:33:19 +0600

    Navbar profile navigation and elementary transaction commit

--- client/src/pages/Financials/transactions/UserTransactions.jsx
+++ client/src/pages/Financials/transactions/UserTransactions.jsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion';
 import { WALLET_EXPENSE_TYPES, WALLET_INCOME_TYPES } from '../../../constants';
 import TransactionIcon from '../../../components/Cards/Transaction.icon';
 import ToggleButton from '../../../components/Buttons/ToggleButton';
+import Alert from '../../../messages/Alert';
 
 const gridSquareVariants = {
   hidden: { opacity: 0 },
@@ -16,6 +17,18 @@ const UserTransactions = () => {
   const handleTransactionToggle = () => {
     setShowExpense(!showExpense);
   };
+
+  const handleCashflow = (e) => {
+    e.preventDefault();
+    console.log(amount, note);
+  };
+
+  // Error state
+  const [error, setError] = useState(null);
+
+  // Form data states
+  const [amount, setAmount] = useState(0);
+  const [note, setNote] = useState("");
 
   return (
     <div className='layoutSection text-slate-200 border-b border-neutral-900 pb-4'>
@@ -84,47 +97,24 @@ const UserTransactions = () => {
 
           <button onClick={handleTransactionToggle} className='text-red-500'>Change</button>
 
-          {showExpense && (
-            <div className="text-slate-900 pb-4 items-center py-4 my-4 lg:items-start">
-              <motion.div
-                variants={gridSquareVariants}
-                className="flex flex-wrap justify-center lg:justify-start"
-              >
-                {WALLET_EXPENSE_TYPES.map((expense, index) => (
-                  <motion.div
-                    key={index}
-                    className="w-full lg:w-1/2 items-center justify-center"
-                    initial={{ opacity: 0, y: 100 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 1, ease: 'easeOut', delay: 0.2 }}
-                  >
-                    <TransactionIcon name={expense.name} icon={expense.icon} />
-                  </motion.div>
-                ))}
-              </motion.div>
-            </div>
-          )}
+          <form onSubmit={handleCashflow} className="justify-center items-center  text-slate-700">
 
-          {!showExpense && (
-            <div className="text-slate-900 pb-4 items-center py-4 my-4 lg:items-start">
-              <motion.div
-                variants={gridSquareVariants}
-                className="flex flex-wrap justify-center lg:justify-start"
-              >
-                {WALLET_INCOME_TYPES.map((income, index) => (
-                  <motion.div
-                    key={index}
-                    className="w-full lg:w-1/2 items-center justify-center"
-                    initial={{ opacity: 0, y: 100 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 1, ease: 'easeOut', delay: 0.2 }}
-                  >
-                    <TransactionIcon name={income.name} icon={income.icon} />
-                  </motion.div>
-                ))}
-              </motion.div>
-            </div>
-          )}
+            <input type="number" 
+                   placeholder="Amount" 
+                   className="input" 
+                   autoFocus 
+                   value={amount} 
+                   onChange={(e) => setAmount(e.target.value)} />
+
+            <input type="text"
+                   placeholder="Add note" 
+                   className="input" 
+                   value={note}
+                   onChange={(e) => setNote(e.target.value)} />
+
+            <button type="submit" className="btn">Add</button>
+            {error && <Alert  msg={error}/>}
+        </form>
         </motion.div>
       </motion.div>
     </div>
--- client/src/sections/Navbar.jsx
+++ client/src/sections/Navbar.jsx
@@ -3,64 +3,68 @@
 import React, { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { UserContext } from '../contexts/UserContext';
+import HeroImage from '../assets/images/Profile.jpg';
 
-const Navbar = ({scrollToAboutMe, scrollToProjects, scrollToContact}) => {
-
-    const handleTargetContact = () => {
-        if (scrollToContact.current) {
-          scrollToContact.current.scrollIntoView({ behavior: 'smooth' });
-        }
-      };
-
-    const {user, setUser} = useContext(UserContext);
-
-    const navigate = useNavigate();
-
-    const handleLogOut = () =>{
-        
-        if(confirm("Are you sure you want to log out?")){
-
-            setUser({
-                name:null, email:null
-            });
-    
-            localStorage.removeItem('email');
-            localStorage.removeItem('webToken');
-    
-            navigate("/");
-
-        }
+const Navbar = ({ scrollToAboutMe, scrollToProjects, scrollToContact }) => {
+  const handleTargetContact = () => {
+    if (scrollToContact.current) {
+      scrollToContact.current.scrollIntoView({ behavior: 'smooth' });
     }
+  };
+
+  const { user, setUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const handleLogOut = () => {
+    if (confirm("Are you sure you want to log out?")) {
+      setUser({
+        name: null,
+        email: null
+      });
+
+      localStorage.removeItem('email');
+      localStorage.removeItem('webToken');
+
+      navigate("/");
+    }
+  }
 
   return (
     <div>
-      
       <header className="bg-indigo-500 text-white fixed w-full top-0 z-10">
+        <nav className="flex items-center justify-between p-4">
+          <div className="flex items-center gap-2">
+            <Link title="Home" to="/" className="fa-solid fa-house-chimney nav-link"></Link>
+            <Link title="Home" to="/"><span>FinanSage</span></Link>
 
-            <nav className="flex items-center justify-between p-4">
-                <div className="flex items-center gap-2">
-                    <Link title="Home" to="/" className="fa-solid fa-house-chimney nav-link"></Link>
-                    <Link title="Home" to="/"><span>FinanSage</span></Link>
-                </div>
+            {user.email && (
+              <span className="inline-block ml-2">
+                <Link title="Visit Profile" to="/user/profile">
+                <img 
+                  src={HeroImage} 
+                  alt='Ishrak Adit'
+                  className='h-6 w-6 object-cover rounded-full shadow-lg' 
+                />
+              </Link>
+              </span>
+            )}
+          </div>
 
-                {user.email ? (
-                    <div className="flex items-center gap-2">
-                        <Link title="Profile" to="user/profile" className="fa-solid fa-user nav-link"></Link>
-                        <Link title="Dashboard" to="user/dashboard" className="fa-solid fa-user nav-link"></Link>
-                        <Link title="Wallets" to="user/wallets" className="fa-solid fa-circle-plus nav-link"></Link>
-                        <button title='Log out' onClick={handleLogOut} className='fa-solid fa-right-from-bracket nav-link'></button>
-                    </div>
-                ) : (
-                    <div className="flex items-center gap-2">
-                        <Link title="Log in" to="/login" className="fa-solid fa-right-to-bracket nav-link"></Link>
-                        <Link title="Register" to="/register" className="fa-solid fa-registered nav-link"></Link>
-                    </div>
-                )}
-
-            </nav>
-
-        </header>
-
+          {user.email ? (
+            <div className="flex items-center gap-2">
+              <Link title="Profile" to="user/profile" className="fa-solid fa-user nav-link"></Link>
+              <Link title="Dashboard" to="user/dashboard" className="fa-solid fa-user nav-link"></Link>
+              <Link title="Wallets" to="user/wallets" className="fa-solid fa-circle-plus nav-link"></Link>
+              <button title='Log out' onClick={handleLogOut} className='fa-solid fa-right-from-bracket nav-link'></button>
+            </div>
+          ) : (
+            <div className="flex items-center gap-2">
+              <Link title="Log in" to="/login" className="fa-solid fa-right-to-bracket nav-link"></Link>
+              <Link title="Register" to="/register" className="fa-solid fa-registered nav-link"></Link>
+            </div>
+          )}
+        </nav>
+      </header>
     </div>
   );
 }
