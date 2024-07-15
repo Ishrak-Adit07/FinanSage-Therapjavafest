f1cfaede6ad9272e74e43b06e6bd39cf5483c90b
Ishrak Adit <ishrak.adit07@gmail.com>
2024-07-15 22:45:12 +0600

    Server cash flow type edit controller commit

--- client/src/controllers/cashflow.controller.js
+++ client/src/controllers/cashflow.controller.js
@@ -1,105 +1,137 @@
 const createCashFlow = async (userID, walletID, cashFlow) => {
-    if (!userID || !walletID || !cashFlow) {
-      throw Error("All fields are required");
-    }
-  
-    try {
-      const response = await fetch("/api/user/cashFlow/create", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ userID, walletID, cashFlow }),
-      });
-  
-      const responseData = await response.json();
-      if (!responseData.success) {
-        throw Error(responseData.error);
-      }
-  
-      return responseData;
-    } catch (error) {
-      console.error("Error:", error.message);
-      throw Error(error.message);
-    }
-  };
+  if (!userID || !walletID || !cashFlow) {
+    throw Error("All fields are required");
+  }
 
-  const editCashFlowAmount = async (userID, walletID, cashFlowID, newAmount) => {
-    if (!userID || !walletID || !cashFlowID || !newAmount) {
-      throw Error("All fields are required");
-    }
-  
-    try {
-      const response = await fetch("/api/user/cashFlow/edit/amount", {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ userID, walletID, cashFlowID, newAmount }),
-      });
-  
-      const responseData = await response.json();
-      if (!responseData.success) {
-        throw Error(responseData.error);
-      }
-  
-      return responseData;
-    } catch (error) {
-      console.error("Error:", error.message);
-      throw Error(error.message);
-    }
-  };
+  try {
+    const response = await fetch("/api/user/cashFlow/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userID, walletID, cashFlow }),
+    });
 
-  const editCashFlowNote = async (userID, walletID, cashFlowID, newNote) => {
-    if (!userID || !walletID || !cashFlowID || !newNote) {
-      throw Error("All fields are required");
+    const responseData = await response.json();
+    if (!responseData.success) {
+      throw Error(responseData.error);
     }
-  
-    try {
-      const response = await fetch("/api/user/cashFlow/edit/note", {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ userID, walletID, cashFlowID, newNote }),
-      });
-  
-      const responseData = await response.json();
-      if (!responseData.success) {
-        throw Error(responseData.error);
-      }
-  
-      return responseData;
-    } catch (error) {
-      console.error("Error:", error.message);
-      throw Error(error.message);
-    }
-  };
-  
-  const deleteCashFlow = async (userID, walletID, cashFlowID) => {
-    if (!userID || !walletID || !cashFlowID) {
-      throw Error("All fields are required");
-    }
-  
-    try {
-      const response = await fetch("/api/user/cashFlow/delete", {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ userID, walletID, cashFlowID }),
-      });
-  
-      const responseData = await response.json();
-      if (!responseData.success) {
-        throw Error(responseData.error);
-      }
-  
-      return responseData;
-    } catch (error) {
-      console.error("Error:", error.message);
-      throw Error(error.message);
-    }
-  };
 
-  export { createCashFlow, deleteCashFlow, editCashFlowAmount, editCashFlowNote }
+    return responseData;
+  } catch (error) {
+    console.error("Error:", error.message);
+    throw Error(error.message);
+  }
+};
+
+const editCashFlowAmount = async (userID, walletID, cashFlowID, newAmount) => {
+  if (!userID || !walletID || !cashFlowID || !newAmount) {
+    throw Error("All fields are required");
+  }
+
+  try {
+    const response = await fetch("/api/user/cashFlow/edit/amount", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userID, walletID, cashFlowID, newAmount }),
+    });
+
+    const responseData = await response.json();
+    if (!responseData.success) {
+      throw Error(responseData.error);
+    }
+
+    return responseData;
+  } catch (error) {
+    console.error("Error:", error.message);
+    throw Error(error.message);
+  }
+};
+
+const editCashFlowNote = async (userID, walletID, cashFlowID, newNote) => {
+  if (!userID || !walletID || !cashFlowID || !newNote) {
+    throw Error("All fields are required");
+  }
+
+  try {
+    const response = await fetch("/api/user/cashFlow/edit/note", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userID, walletID, cashFlowID, newNote }),
+    });
+
+    const responseData = await response.json();
+    if (!responseData.success) {
+      throw Error(responseData.error);
+    }
+
+    return responseData;
+  } catch (error) {
+    console.error("Error:", error.message);
+    throw Error(error.message);
+  }
+};
+
+const editCashFlowType = async (userID, walletID, cashFlowID, newType) => {
+  if (!userID || !walletID || !cashFlowID || !newType) {
+    throw Error("All fields are required");
+  }
+
+  try {
+    const response = await fetch("/api/user/cashFlow/edit/type", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userID, walletID, cashFlowID, newType }),
+    });
+
+    const responseData = await response.json();
+    if (!responseData.success) {
+      throw Error(responseData.error);
+    }
+
+    return responseData;
+  } catch (error) {
+    console.error("Error:", error.message);
+    throw Error(error.message);
+  }
+};
+
+const deleteCashFlow = async (userID, walletID, cashFlowID) => {
+  if (!userID || !walletID || !cashFlowID) {
+    throw Error("All fields are required");
+  }
+
+  try {
+    const response = await fetch("/api/user/cashFlow/delete", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userID, walletID, cashFlowID }),
+    });
+
+    const responseData = await response.json();
+    if (!responseData.success) {
+      throw Error(responseData.error);
+    }
+
+    return responseData;
+  } catch (error) {
+    console.error("Error:", error.message);
+    throw Error(error.message);
+  }
+};
+
+export {
+  createCashFlow,
+  deleteCashFlow,
+  editCashFlowAmount,
+  editCashFlowNote,
+  editCashFlowType,
+};
