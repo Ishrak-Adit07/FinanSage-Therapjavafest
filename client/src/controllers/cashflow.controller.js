54fc00c9c14a362a307276b9fe74c76c87fc26fe
Ishrak Adit <ishrak.adit07@gmail.com>
2024-07-15 22:43:51 +0600

    Client cash flow note edit controller commit

--- client/src/controllers/cashflow.controller.js
+++ client/src/controllers/cashflow.controller.js
@@ -49,6 +49,32 @@ const createCashFlow = async (userID, walletID, cashFlow) => {
       throw Error(error.message);
     }
   };
+
+  const editCashFlowNote = async (userID, walletID, cashFlowID, newNote) => {
+    if (!userID || !walletID || !cashFlowID || !newNote) {
+      throw Error("All fields are required");
+    }
+  
+    try {
+      const response = await fetch("/api/user/cashFlow/edit/note", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userID, walletID, cashFlowID, newNote }),
+      });
+  
+      const responseData = await response.json();
+      if (!responseData.success) {
+        throw Error(responseData.error);
+      }
+  
+      return responseData;
+    } catch (error) {
+      console.error("Error:", error.message);
+      throw Error(error.message);
+    }
+  };
   
   const deleteCashFlow = async (userID, walletID, cashFlowID) => {
     if (!userID || !walletID || !cashFlowID) {
@@ -76,4 +102,4 @@ const createCashFlow = async (userID, walletID, cashFlow) => {
     }
   };
 
-  export { createCashFlow, deleteCashFlow, editCashFlowAmount }
+  export { createCashFlow, deleteCashFlow, editCashFlowAmount, editCashFlowNote }
