bc6773e1e4cf41434b74f93e59cf635f3b26c8cf
Ishrak Adit <ishrak.adit07@gmail.com>
2024-07-15 22:38:36 +0600

    Client cash flow creation and deletion controller commit

---
+++ client/src/controllers/cashflow.controller.js
@@ -0,0 +1,53 @@
+const createCashFlow = async (userID, walletID, cashFlow) => {
+    if (!userID || !walletID || !cashFlow) {
+      throw Error("All fields are required");
+    }
+  
+    try {
+      const response = await fetch("/api/user/cashFlow/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userID, walletID, cashFlow }),
+      });
+  
+      const responseData = await response.json();
+      if (!responseData.success) {
+        throw Error(responseData.error);
+      }
+  
+      return responseData;
+    } catch (error) {
+      console.error("Error:", error.message);
+      throw Error(error.message);
+    }
+  };
+  
+  const deleteCashFlow = async (userID, walletID, cashFlowID) => {
+    if (!userID || !walletID || !cashFlowID) {
+      throw Error("All fields are required");
+    }
+  
+    try {
+      const response = await fetch("/api/user/cashFlow/delete", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userID, walletID, cashFlowID }),
+      });
+  
+      const responseData = await response.json();
+      if (!responseData.success) {
+        throw Error(responseData.error);
+      }
+  
+      return responseData;
+    } catch (error) {
+      console.error("Error:", error.message);
+      throw Error(error.message);
+    }
+  };
+
+  export { createCashFlow, deleteCashFlow }
