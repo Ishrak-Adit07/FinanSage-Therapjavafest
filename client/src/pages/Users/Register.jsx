85c6f6bbd1fa5aae0a36e1baa72594d976807094
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-02-12 15:35:26 +0600

    Fix commit: Registration page design

--- client/src/pages/Users/Register.jsx
+++ client/src/pages/Users/Register.jsx
@@ -5,7 +5,6 @@ import RegisterForm from "../../components/Forms/RegisterForm";
 const Register = () => {
   return (
     <section className="mt-20 pb-20">
-      <h1>This is merchant registration</h1>
       <RegisterForm />
       <h1 className="text-indigo-500 text-bold text-center">
         Copyright@FinanSage.Official
